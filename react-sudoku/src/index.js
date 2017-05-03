956608f6022a16449ecc31255884609711ddcbd5
David Edwards <dedwards@pivotal.io>
2017-05-03 15:25:47 -0400

    Initial commit
    Basic scaffold with high-level policy module, plus gui module built from
    create-react-app

---
+++ react-sudoku/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './gui/App';
+import './gui/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
